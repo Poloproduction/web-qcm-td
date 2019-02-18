3676e5aef950550e380ab251af4952eb515b21b9
Paul CUVILLIER <paul.poloproduction@hotmail.fr>
2019-02-18 12:09:10 +0100

    Add ds exo 7 finished

--- exercice/exercice7.php
+++ exercice/exercice7.php
@@ -6,10 +6,25 @@
 
 $N = array_shift($args);
 $K = array_shift($args);
-for ($i = 0; $i < $N; $i++) {
+$score = 0;
+
+for($i = 0; $i < $N; $i++) {
     $A = $args[$i];
+    if(strpos($A, "X") === 0 && $K[$i] === 'A') {
+        $score += (100/(int)$N);
+    } elseif(strpos($A, "X") === 1 && $K[$i] === 'B') {
+        $score += (100/(int)$N);
+    } elseif(strpos($A, "X") === 2 && $K[$i] === 'C') {
+        $score += (100/(int)$N);
+    } elseif(strpos($A, "X") === 3 && $K[$i] === 'D') {
+        $score += (100/(int)$N);
+    } elseif(strpos($A, "X") === 4 && $K[$i] === 'E') {
+        $score += (100/(int)$N);
+    }
 }
 
 // Write an action using echo().
 
-echo("answer");
+echo($score);
+
+
