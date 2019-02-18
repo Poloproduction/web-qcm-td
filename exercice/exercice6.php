d31fe503591bac98cd26ba5835164808f761b1c4
Paul CUVILLIER <paul.poloproduction@hotmail.fr>
2019-02-18 11:32:36 +0100

    Add ds exo 6 finished

--- exercice/exercice6.php
+++ exercice/exercice6.php
@@ -7,5 +7,9 @@
 $M = array_shift($args);
 for ($i = 0; $i < $M; $i++) {
     $N = $args[$i];
+    if($N%2 == 0) {
+        $N += 1;
+    }
     echo "{$N}\n";
+    //Sous Windows: echo "{$N}\r\n";
 }
